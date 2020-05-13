9eba870c4c2d4e414f213df3c608c66a53a365f1
vzvz4 <vzvz4@mail.ru>
2020-05-13 22:39:44 +0900

    key listener

--- core/src/com/mygdx/game/screen/GameScreen.java
+++ core/src/com/mygdx/game/screen/GameScreen.java
@@ -1,22 +1,38 @@
 package com.mygdx.game.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import com.mygdx.game.base.BaseScreen;
 import com.mygdx.game.math.Rect;
 import com.mygdx.game.sprite.Background;
+import com.mygdx.game.sprite.Ship;
+import com.mygdx.game.sprite.Star;
 
 public class GameScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
+    private Ship ship;
+    private TextureAtlas mainGameAtlas;
+    private TextureAtlas atlas;
+    private Star[] stars;
+    private int keycode;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("cyan gradient.png");
         background = new Background(bg);
+        mainGameAtlas = new TextureAtlas(Gdx.files.internal("textures/mainAtlas.tpack"));
+        atlas = new TextureAtlas(Gdx.files.internal("textures/menuAtlas.tpack"));
+        ship = new Ship(mainGameAtlas);
+        stars = new Star[256];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
     }
 
     @Override
@@ -29,21 +45,30 @@ public class GameScreen extends BaseScreen {
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        ship.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        mainGameAtlas.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
     @Override
     public boolean keyDown(int keycode) {
+        this.keycode = keycode;
+        ship.keyDown();
         return super.keyDown(keycode);
     }
 
     @Override
     public boolean keyUp(int keycode) {
+        ship.keyUp();
         return super.keyUp(keycode);
     }
 
@@ -54,16 +79,25 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
+        ship.touchUp(touch, pointer, button);
         return super.touchUp(touch, pointer, button);
     }
 
     private void update(float delta) {
-
+        for (Star star : stars) {
+            star.update(delta);
+        }
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        ship.draw(batch);
+        if (!ship.isPressed())
+            ship.keyListener(keycode);
         batch.end();
     }
 }
---
+++ core/src/com/mygdx/game/screen/KeySet.java
@@ -0,0 +1,23 @@
+package com.mygdx.game.screen;
+
+public enum KeySet {
+    UP(51,19),
+    DOWN(47, 20),
+    LEFT(29,21),
+    RIGHT(32,22);
+
+    private int[] keycode;
+
+    KeySet(int ... keyCode) {
+        this.keycode = keyCode;
+    }
+
+    //как то можно включить поддержку stream ?
+    public boolean contain(int key) {
+        for (int k:keycode) {
+            if (k == key)
+                return true;
+        }
+        return false;
+    }
+}
